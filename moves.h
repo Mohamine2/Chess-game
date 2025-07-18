d5aba8b160cf678bdeb13b03f5852c3eb389b00f
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2025-07-18 21:38:32 +0200

    Function organization

--- moves.h
+++ moves.h
@@ -2,6 +2,9 @@
 #define MOVES_H
 
 int secureScanf(const char *message, int min, int max);
+void bigPawnMoves(char board[8][8],int x_pos,int y_pos);
+void smallPawnMoves(char board[8][8],int x_pos,int y_pos);
+void knightMoves(char board[8][8],int x_pos,int y_pos);
 void moveFinder(char board[8][8]);
 
 #endif
